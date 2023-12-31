74aa026c930a4bc3bb51cfe40db8df57d0c51a17
tajo <vojtech@miksu.cz>
2023-12-31 19:19:10 +0900

    fix

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,7 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   ladleShots: {
-    baseUrl: "http://172.17.0.1:61000",
+    baseUrl: "http://localhost:61000",
   },
   lostPixelProjectId: "clqtbczr03obzmh0epne5a5t5",
   apiKey: process.env.LOST_PIXEL_API_KEY,
