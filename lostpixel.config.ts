fa30781fb044c14bd78414c884c9a99416af1662
Vojtech Miksu <vojtech@miksu.cz>
2024-01-03 12:09:14 +0900

    Apply suggestions from code review
    Co-authored-by: Dima Ivashchuk <dimaiv@me.com>

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,7 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   ladleShots: {
-    baseUrl: "http://localhost:61000",
+      ladleUrl: './build',
   },
   lostPixelProjectId: "clqtbczr03obzmh0epne5a5t5",
   apiKey: process.env.LOST_PIXEL_API_KEY,
